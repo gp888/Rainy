75fd8921cff466da18e2cec047b1286e2846b5ca
gp_dbn_nxin <18832868180@163.com>
2019-04-01 14:21:13 +0800

    launchActivity全屏

--- app/src/main/java/com/gp/rainy/LaunchActivity.java
+++ app/src/main/java/com/gp/rainy/LaunchActivity.java
@@ -1,18 +1,37 @@
 package com.gp.rainy;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.VideoView;
+
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= LOLLIPOP) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        int flag= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        Window window = getWindow();
+        window.setFlags(flag, flag);
+
         setContentView(R.layout.activity_launch);
         VideoView view = findViewById(R.id.video);
         String uri = "android.resource://" + getPackageName() + "/" + R.raw.yigou;
